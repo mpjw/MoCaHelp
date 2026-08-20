6220e1bd2e61331665508629ebd07e4646ab16c8
Marcus Wagner <marcus.wagner@tum.de>
2026-08-20 12:32:41 +0200

    fix #3 by implementing load_genomic_regions() to serve as exported function for lazy loading genomic regions from reference genomes with default patch per species

--- R/data.R
+++ R/data.R
@@ -47,3 +47,58 @@
 #' }
 #' @source <http://genome.ucsc.edu/cgi-bin/hgTables>
 "GRCh38.p12.mappability"
+
+#' Loads genomic ancillary regions
+#'
+#' This function loads genomic regions depending on a species (and reference
+#' genome version). Available options are chromosomes, centromeres, alternative
+#' haplotypes, and regions of poor mappability.
+#'
+#' @param species Character name of species, i.e. "human" or "mouse".
+#' @param region_type Character specifying the type of regions to return. I.e.
+#'  one of "chromosomes", "centromeres", "alt.haplotypes", or "mappability".
+#' @param ref_genome Character name of reference genome to use for species. Per
+#'  default (NULL) GRCh38.p12 and GRCm38.p6 will be used.
+#' @param return_granges Logical if to return results as GenomicRanges, returns
+#'  a data.table by default (FALSE).
+#' @returns \code{data.table::data.table} or \code{GenomicRanges::GRanges}
+#'  listing genomic regions.
+#' @importFrom utils data
+#' @importFrom GenomicRanges makeGRangesFromDataFrame
+#' @export
+load_genomic_regions <- function(
+  species,
+  region_type,
+  ref_genome = NULL,
+  return_granges = FALSE
+) {
+  stopifnot(
+    region_type %in%
+      c("chromosomes", "centromeres", "alt.haplotypes", "mappability")
+  )
+
+  # determine reference genome
+  if (is.null(ref_genome)) {
+    ref_genome <- switch(species, human = "GRCh38.p12", mouse = "GRCm38.p6")
+  }
+
+  stopifnot(
+    ref_genome %in% c("GRCh38.p12", "GRCm38.p6")
+  )
+
+  # load data
+  .env <- new.env()
+  data_set <- paste0(ref_genome, ".", region_type)
+  utils::data(list = data_set, envir = .env)[1]
+  regions <- .env[[data_set]]
+
+  # convert to genomic ranges object if requested
+  if (return_granges) {
+    regions <- GenomicRanges::makeGRangesFromDataFrame(
+      regions,
+      keep.extra.columns = TRUE
+    )
+  }
+
+  regions
+}
