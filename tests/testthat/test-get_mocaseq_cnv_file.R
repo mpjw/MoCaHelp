a7fb399995663282608068546f6aa147a68cced0
Marcus Wagner <marcus.wagner@tum.de>
2026-08-21 15:07:46 +0200

    adapting tests to new functionality; add tests for detect_mocaseq_version and detect_sample_mode

--- tests/testthat/test-get_mocaseq_cnv_file.R
+++ tests/testthat/test-get_mocaseq_cnv_file.R
@@ -1,25 +1,37 @@
-test_that("get_mocaseq_cnv_file resolves paths correctly", {
-  sample_id <- "PCSI_0509_Pa_P"
+test_that("get_mocaseq_cnv_file resolves paths correctly for matched sample", {
+  sample_id <- "SAMPLE_042_TUMOR"
 
   # copy number segment files
   result_type <- "segments"
   expect_equal(
-    get_mocaseq_cnv_file(sample_id, "matched", "HMMCopy", result_type),
+    get_mocaseq_cnv_file(
+      sample_id,
+      "matched",
+      "matched",
+      "HMMCopy",
+      result_type
+    ),
     paste0(sample_id, ".HMMCopy.20000.segments.txt")
   )
 
   expect_equal(
-    get_mocaseq_cnv_file(sample_id, "matched", "Copywriter", result_type),
+    get_mocaseq_cnv_file(
+      sample_id,
+      "matched",
+      "matched",
+      "Copywriter",
+      result_type
+    ),
     paste0(sample_id, ".Copywriter.segments.Mode.txt")
   )
 
   expect_equal(
-    get_mocaseq_cnv_file(sample_id, "Tumor", "CNVKit", result_type),
-    file.path("Tumor", paste0(sample_id, ".cns"))
+    get_mocaseq_cnv_file(sample_id, "matched", "Tumor", "CNVKit", result_type),
+    file.path("matched", paste0(sample_id, ".Tumor", ".cns"))
   )
 
   expect_error(
-    get_mocaseq_cnv_file(sample_id, "foo", "Copywriter", result_type)
+    get_mocaseq_cnv_file(sample_id, "matched", "foo", "Copywriter", result_type)
   )
 
   # copy number ratio files
@@ -27,6 +39,7 @@ test_that("get_mocaseq_cnv_file resolves paths correctly", {
   expect_equal(
     get_mocaseq_cnv_file(
       sample_id,
+      "matched",
       "matched",
       "HMMCopy",
       result_type,
@@ -36,16 +49,93 @@ test_that("get_mocaseq_cnv_file resolves paths correctly", {
   )
 
   expect_equal(
-    get_mocaseq_cnv_file(sample_id, "Normal", "Copywriter", result_type),
+    get_mocaseq_cnv_file(
+      sample_id,
+      "matched",
+      "Normal",
+      "Copywriter",
+      result_type
+    ),
     paste0(sample_id, ".Copywriter.log2RR.Mode.txt")
   )
 
   expect_equal(
-    get_mocaseq_cnv_file(sample_id, "Tumor", "CNVKit", result_type),
-    file.path("Tumor", paste0(sample_id, ".cnr"))
+    get_mocaseq_cnv_file(
+      sample_id,
+      "matched",
+      "matched",
+      "CNVKit",
+      result_type
+    ),
+    file.path("matched", paste0(sample_id, ".cnr"))
   )
 
   expect_error(
-    get_mocaseq_cnv_file(sample_id, "foo", "CNVKit", result_type)
+    get_mocaseq_cnv_file(sample_id, "foo", "matched", "CNVKit", result_type)
+  )
+})
+
+test_that("get_mocaseq_cnv_file resolves paths correctly for single sample", {
+  sample_id <- "SAMPLE_042_TUMOR"
+
+  # copy number segment files
+  result_type <- "segments"
+  expect_equal(
+    get_mocaseq_cnv_file(sample_id, "single", "Tumor", "HMMCopy", result_type),
+    paste0(sample_id, ".HMMCopy.20000.segments.txt")
+  )
+
+  expect_equal(
+    get_mocaseq_cnv_file(
+      sample_id,
+      "single",
+      "Tumor",
+      "Copywriter",
+      result_type
+    ),
+    paste0(sample_id, ".Copywriter.segments.Mode.txt")
+  )
+
+  expect_equal(
+    get_mocaseq_cnv_file(sample_id, "single", "Tumor", "CNVKit", result_type),
+    file.path("single", paste0(sample_id, ".Tumor", ".cns"))
+  )
+
+  # copy number ratio files
+  result_type <- "ratios"
+  expect_equal(
+    get_mocaseq_cnv_file(
+      sample_id,
+      "single",
+      "Tumor",
+      "HMMCopy",
+      result_type,
+      seg_size = 1000
+    ),
+    paste0(sample_id, ".HMMCopy.1000.log2RR.txt")
+  )
+
+  expect_equal(
+    get_mocaseq_cnv_file(
+      sample_id,
+      "single",
+      "Tumor",
+      "Copywriter",
+      result_type
+    ),
+    paste0(sample_id, ".Copywriter.log2RR.Mode.txt")
+  )
+
+  expect_equal(
+    get_mocaseq_cnv_file(sample_id, "single", "Tumor", "CNVKit", result_type),
+    file.path("single", paste0(sample_id, ".Tumor", ".cnr"))
+  )
+
+  expect_error(
+    get_mocaseq_cnv_file(sample_id, "single", "foo", "CNVKit", result_type)
+  )
+
+  expect_error(
+    get_mocaseq_cnv_file(sample_id, "foo", "Tumor", "Copywriter", result_type)
   )
 })
